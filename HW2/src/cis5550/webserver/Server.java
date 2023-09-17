6700619110807cc2f047b3639fb03ad8d374377b
Yingqiu Liu <yingqiul@seas.upenn.edu>
2023-09-17 11:53:36 -0700

    adding more fields and APIs

--- HW2/src/cis5550/webserver/Server.java
+++ HW2/src/cis5550/webserver/Server.java
@@ -1,6 +1,7 @@
 package cis5550.webserver;
 
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 
@@ -9,26 +10,35 @@ import com.sun.net.httpserver.HttpServer;
 
 import static java.lang.System.exit;
 
-public class Server {
+public class Server implements Runnable {
     static Logger log = Logger.getLogger(Server.class);
     final static String SERVER_NAME = "hw-coco";
 
-    static int port = 80;
+    static Server server = null;
+    static boolean threadLaunched = false;
+
+    int port = 80;
+    String path = "";
     public Server() {
     }
 
     static final int NUM_WORKERS = 100;
-    public static void main(String[] args) throws Exception {
-        String path = "";
-        if (args.length == 2) {
-            port = Integer.parseInt(args[0]);
-            path = args[1];
-        } else {
-            System.out.println("Written by Yingqiu Liu");
-            exit(0);
+    public void run() {
+//        String path = "";
+//        if (args.length == 2) {
+//            port = Integer.parseInt(args[0]);
+//            path = args[1];
+//        } else {
+//            System.out.println("Written by Yingqiu Liu");
+//            exit(0);
+//        }
+        HttpServer server = null;
+        try {
+            server = HttpServer.create(new InetSocketAddress(this.port), 0);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.createContext("/", new CISHttpHandler(path));
+        server.createContext("/", new CISHttpHandler(this.path));
         try {
             // TODO: if seeing degraded performance, considering use newCachedThreadPool() instead.
             server.setExecutor(Executors.newFixedThreadPool(NUM_WORKERS));
@@ -41,18 +51,53 @@ public class Server {
     }
 
     public static void port(int N) {
-        port = N;
+        if (server != null) {
+            server.port = N;
+        } else {
+            log.error("Server hasn't been initialized for taking port");
+        }
     }
 
     public static class staticFiles {
         public static void location(String s) {
-
+            if (server != null) {
+                server.path = s;
+            } else {
+                log.error("Server hasn't been initialized for taking file path");
+            }
         }
     }
 
-    public static void get(String s, Route r) {}
+    public static void get(String s, Route r) {
+        if (server == null) {
+            server = new Server();
+        }
+        if (!threadLaunched) {
+            Thread t = new Thread(server);
+            t.start();
+            threadLaunched = true;
+        }
+    }
 
-    public static void post(String s, Route r){}
+    public static void post(String s, Route r){
+        if (server == null) {
+            server = new Server();
+        }
+        if (!threadLaunched) {
+            Thread t = new Thread(server);
+            t.start();
+            threadLaunched = true;
+        }
+    }
 
-    public static void put(String s, Route r){}
+    public static void put(String s, Route r){
+        if (server == null) {
+            server = new Server();
+        }
+        if (!threadLaunched) {
+            Thread t = new Thread(server);
+            t.start();
+            threadLaunched = true;
+        }
+    }
 }
