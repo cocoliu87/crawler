c3724f9dd92e9dda5f31af46cd4964ca14252bf9
Yingqiu Liu <yingqiul@seas.upenn.edu>
2023-10-27 23:05:08 -0700

    working on robot file

--- HW8/src/cis5550/jobs/Crawler.java
+++ HW8/src/cis5550/jobs/Crawler.java
@@ -13,8 +13,8 @@ import javax.swing.text.html.parser.ParserDelegator;
 import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
-import java.sql.Array;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Crawler {
     static final String crawlerTable = "pt-crawl", hostsTable = "hosts", timeCol = "time", robotCol = "robot", agent = "cis5550-crawler";
@@ -23,7 +23,7 @@ public class Crawler {
         ctx.output(args.length == 1? "OK\n" : "Error: Crawler expects one argument\n");
 
         FlameRDDImpl urlQueue = (FlameRDDImpl) ctx.parallelize(Arrays.asList(args));
-
+        AtomicReference<Double> crawlInterval = new AtomicReference<>(0.1);
         while (urlQueue.count() != 0) {
             urlQueue = (FlameRDDImpl) urlQueue.flatMap(url -> {
                 KVSClient client = ctx.getKVS();
@@ -36,70 +36,123 @@ public class Crawler {
                     getCheckRobot(host, client, hostsTable);
                 }
 
-                if (invalidForCrawl(host)) {
-                    return List.of();
-                }
+                RobotRules robotRules = invalidForCrawl(host);
 
+                // if rules are not null, we need respect the rules
+                if (robotRules != null) {
+                    if (robotRules.CrawlDelay != 0)
+                        crawlInterval.set(robotRules.CrawlDelay);
+                    // agent is not valid to crawl
+                    if (!robotRules.isValidAgent()) {
+                        return List.of();
+                    }
 
-                Map<Integer, String> head = headCheckCode(url);
-                int headCode = 0;
-                String redirect = "";
-                for (Map.Entry<Integer, String> entry: head.entrySet()){
-                    headCode = entry.getKey();
-                    redirect = entry.getValue();
-                }
-                //if (code != 200 && !contentType.equals("text/html")) {
-                if (headCode < 0) {
-                    return List.of();
-                }
-
-                // redirect to url fetched from Location
-                if (new HashSet<>(List.of(301, 302, 303, 307, 308)).contains(headCode)) {
-                    return List.of(redirect);
-                }
-
-                String hostName = URLParser.parseURL(url)[1];
-                byte[] timeBytes = client.get(hostsTable, hostName, timeCol);
-                if (timeBytes != null) {
-                    String lastTime = new String(client.get(hostsTable, hostName, timeCol), StandardCharsets.UTF_8);
-                    if (!lastTime.isEmpty()) {
-                        long lastTimeLong = Long.parseLong(lastTime);
-                        if (lastTimeLong > 0 && System.currentTimeMillis() - lastTimeLong < 100) {
-                            return List.of(url);
+                    String allowed = "", disallowed = "";
+                    int pAllowed = -1, pDisallowed = -1;
+                    AllowedRule allowedRule = robotRules.getAllowedRule();
+                    DisallowedRule disallowedRule = robotRules.getDisallowedRule();
+                    if (allowedRule != null) {
+                        allowed = allowedRule.AllowedHost;
+                        pAllowed = allowedRule.Priority;
+                    }
+                    if (disallowedRule != null) {
+                        disallowed = disallowedRule.DisallowedHost;
+                        pDisallowed = disallowedRule.Priority;
+                    }
+                    boolean matchAllowed = !allowed.isEmpty() && host.startsWith(allowed), matchDisallowed = !disallowed.isEmpty() && host.startsWith(disallowed);
+                    // TODO: check if empty rules is allowing all
+                    // pDisallowed == 1, or pDisallowed = -1
+                    if (pAllowed == 0) {
+                        if (matchAllowed)
+                            return crawlPage(url, client);
+                        else
+                            return List.of();
+                    }
+                    // pAllowed == 1 or pAllowed = -1
+                    if (pDisallowed == 0) {
+                        if (matchDisallowed)
+                            return List.of();
+                        else {
+                            if (pAllowed == 1) {
+                                if (matchAllowed) {
+                                    return crawlPage(url, client);
+                                } else {
+                                    return List.of();
+                                }
+                            } else {
+                                return crawlPage(url, client);
+                            }
                         }
                     }
-                }
-                client.put("hosts", hostName, "time", String.valueOf(System.currentTimeMillis()));
 
-                HttpURLConnection conn;
-                conn = (HttpURLConnection) (new URI(url).toURL()).openConnection();
-                conn.setRequestMethod("GET");
-                conn.setRequestProperty("User-Agent", agent);
-                conn.setRequestProperty("Content-Type", "text/html");
-                conn.connect();
-                int responseCode = conn.getResponseCode();
-                List<String> links = new ArrayList<>();
-                if (responseCode == 200) {
-                    InputStreamReader input = new InputStreamReader((InputStream) conn.getContent());
-                    BufferedReader reader = new BufferedReader(input);
-                    StringBuilder page = new StringBuilder();
-                    String line;
-                    do {
-                        line = reader.readLine();
-                        page.append(line);
-                    } while (line != null);
-                    Row r = new Row(Hasher.hash(url));
-                    r.put("url", url);
-                    r.put("page", page.toString());
-
-                    client.putRow(crawlerTable, r);
-                    input.close();
-                    links = processUrls(getAllUrls(page.toString()), url);
+                    //both pAllowed and pDisallowed == -1 (unset)
+                    return crawlPage(url, client);
+                } else {
+                    // not found robot file
+                    return crawlPage(url, client);
                 }
-                return links;
             });
-            Thread.sleep(100);
+            Thread.sleep((long) (crawlInterval.get()*1000));
         }
+    }
+
+    public static List<String> crawlPage(String url, KVSClient client) throws IOException, URISyntaxException {
+        Map<Integer, String> head = headCheckCode(url);
+        int headCode = 0;
+        String redirect = "";
+        for (Map.Entry<Integer, String> entry: head.entrySet()){
+            headCode = entry.getKey();
+            redirect = entry.getValue();
+        }
+        //if (code != 200 && !contentType.equals("text/html")) {
+        if (headCode < 0) {
+            return List.of();
+        }
+
+        // redirect to url fetched from Location
+        if (new HashSet<>(List.of(301, 302, 303, 307, 308)).contains(headCode)) {
+            return List.of(redirect);
+        }
+
+        String hostName = URLParser.parseURL(url)[1];
+        byte[] timeBytes = client.get(hostsTable, hostName, timeCol);
+        if (timeBytes != null) {
+            String lastTime = new String(client.get(hostsTable, hostName, timeCol), StandardCharsets.UTF_8);
+            if (!lastTime.isEmpty()) {
+                long lastTimeLong = Long.parseLong(lastTime);
+                if (lastTimeLong > 0 && System.currentTimeMillis() - lastTimeLong < 100) {
+                    return List.of(url);
+                }
+            }
+        }
+        client.put("hosts", hostName, "time", String.valueOf(System.currentTimeMillis()));
+
+        HttpURLConnection conn;
+        conn = (HttpURLConnection) (new URI(url).toURL()).openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("User-Agent", agent);
+        conn.setRequestProperty("Content-Type", "text/html");
+        conn.connect();
+        int responseCode = conn.getResponseCode();
+        List<String> links = new ArrayList<>();
+        if (responseCode == 200) {
+            InputStreamReader input = new InputStreamReader((InputStream) conn.getContent());
+            BufferedReader reader = new BufferedReader(input);
+            StringBuilder page = new StringBuilder();
+            String line;
+            do {
+                line = reader.readLine();
+                page.append(line);
+            } while (line != null);
+            Row r = new Row(Hasher.hash(url));
+            r.put("url", url);
+            r.put("page", page.toString());
+
+            client.putRow(crawlerTable, r);
+            input.close();
+            links = processUrls(getAllUrls(page.toString()), url);
+        }
+        return links;
     }
 
     public static Map<Integer, String> headCheckCode(String url) throws IOException, URISyntaxException {
@@ -190,6 +243,15 @@ public class Crawler {
         return processed;
     }
 
+    /**
+     * Check if the host has robots file to claim rules for crawling. This method checks the file and push the content to
+     * RDD table and saves to local cache
+     * @param host
+     * @param client
+     * @param tableName
+     * @throws URISyntaxException
+     * @throws IOException
+     */
     public static void getCheckRobot(String host, KVSClient client, String tableName) throws URISyntaxException, IOException {
         String url = host + "/robots.txt";
         HttpURLConnection conn = (HttpURLConnection) (new URI(url).toURL()).openConnection();
@@ -215,27 +277,41 @@ public class Crawler {
         }
     }
 
-    public static boolean invalidForCrawl(String host) {
+    /**
+     * Check if the robots rules
+     * @param host
+     * @return
+     */
+    public static RobotRules invalidForCrawl(String host) {
         if (!robots.containsKey(host)) {
-            return false;
+            return null;
         }
         String[] policies = robots.get(host).split(System.lineSeparator());
-        for (String p: policies) {
-            if (p.contains(":")) {
-                String[] strs = p.split(":");
+        RobotRules rule = new RobotRules();
+        for (String policy: policies) {
+            if (policy.contains(":")) {
+                String[] strs = policy.split(":");
                 String key = strs[0].strip().toLowerCase();
                 if (key.equals("user-agent")) {
                     String value = strs[1].strip();
-                    return value.equals("*") || value.equals(agent);
+                    rule.setValidAgent(value.equals("*") || value.equals(agent));
                 }
                 if (key.equals("disallow")) {
-
+                    int p = 0;
+                    if (rule.getAllowedRule() != null) {
+                        p = 1;
+                    }
+                    rule.setDisallowedRule(new DisallowedRule(strs[1].strip(), p));
                 }
                 if (key.equals("allow")) {
-
+                    int p = 0;
+                    if (rule.getDisallowedRule() != null) {
+                        p = 1;
+                    }
+                    rule.setAllowedRule(new AllowedRule(strs[1].strip(), p));
                 }
             }
         }
-        return true;
+        return rule;
     }
 }
