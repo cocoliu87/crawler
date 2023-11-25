48805f50493536e4b18d8f3c73e9ff26520ce56e
Sergio Garcia <sergiogcx@gmail.com>
2023-11-25 18:29:54 -0500

    Update search API

--- ubercrawl/src/cis5550/api/SearchAPI.java
+++ ubercrawl/src/cis5550/api/SearchAPI.java
@@ -2,12 +2,20 @@ package cis5550.api;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.*;
+
 import static cis5550.webserver.Server.*;
+
+import cis5550.external.PorterStemmer;
 import cis5550.tools.Logger;
+import cis5550.tools.SearchResult;
+import cis5550.tools.TFIDFDocument;
 
 class SearchAPI {
 
     private static final Logger logger = Logger.getLogger(SearchAPI.class);
+
+    private final List<String> stopWords = List.of("a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "arent", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "cant", "cannot", "could", "couldnt", "did", "didnt", "do", "does", "doesnt", "doing", "dont", "down", "during", "each", "few", "for", "from", "further", "had", "hadnt", "has", "hasnt", "have", "havent", "having", "he", "hed", "hell", "hes", "her", "here", "heres", "hers", "herself", "him", "himself", "his", "how", "hows", "i", "id", "ill", "im", "ive", "if", "in", "into", "is", "isnt", "it", "its", "its", "itself", "lets", "me", "more", "most", "mustnt", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shant", "she", "shed", "shell", "shes", "should", "shouldnt", "so", "some", "such", "than", "that", "thats", "the", "their", "theirs", "them", "themselves", "then", "there", "theres", "these", "they", "theyd", "theyll", "theyre", "theyve", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasnt", "we", "wed", "well", "were", "weve", "were", "werent", "what", "whats", "when", "whens", "where", "wheres", "which", "while", "who", "whos", "whom", "why", "whys", "with", "wont", "would", "wouldnt", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves");
 
     private static String mockSearch(String mockDataFile) throws IOException {
         FileInputStream fis = new FileInputStream(mockDataFile);
@@ -20,6 +28,137 @@ class SearchAPI {
         fis.close();
         return sb.toString();
     }
+
+//
+//
+//    private String stemWord(String term) {
+//        PorterStemmer porterStemmer = new PorterStemmer();
+//        char[] charArray = term.toLowerCase().trim().toCharArray();
+//        porterStemmer.add(charArray, charArray.length);
+//        porterStemmer.stem();
+//        return porterStemmer.toString();
+//    }
+//
+//    private String[] getWords(String document) {
+//        String[] firstSet = document
+//                .toLowerCase()
+//                .replaceAll("\\p{Punct}", "")
+//                .split("\\s+");
+//
+//        return Arrays
+//                .stream(firstSet)
+//                .filter(word -> !stopWords.contains(word))
+//                .filter(word -> !word.isEmpty())
+//                .map(this::stemWord)
+//                .toArray(String[]::new);
+//    }
+//
+//
+//    /**
+//     * Calculates tfIdf values for each search term
+//     * @param query - A string containing the search term
+//     * @return
+//     */
+//    public Map<String, Double> vectorize(String query) {
+//        // Stores the word->values in a map
+//        HashMap<String, Double> tfidfVector = new HashMap<>();
+//        // Split query into words
+//        String[] words = getWords(query);
+//
+//        // Compute term frequencies for the query
+//        HashMap<String, Integer> tf = new HashMap<>();
+//        for (String word : words) {
+//            tf.put(word, tf.getOrDefault(word, 0) + 1);
+//        }
+//
+//        // Compute TF-IDF values for each term
+//        for (String term : tf.keySet()) {
+//            double tfidf = (1 + Math.log(tf.get(term))) * idf.getOrDefault(term, 0.0);
+//            tfidfVector.put(term, tfidf);
+//        }
+//
+//        return tfidfVector;
+//    }
+//
+//    private double computeCosineSimilarity(Map<String, Double> vector1, Map<String, Integer> vector2) {
+//        // The sum of the element-wise multiplication of corresponding components of the vectors.
+//        double dotProduct = 0.0;
+//
+//        // The Euclidean norms (magnitude) of the vectors, calculated by summing the squares of their components.
+//        double norm1 = 0.0;
+//        double norm2 = 0.0;
+//
+//        /**
+//         * The loop iterates over the terms in vector1. For each term,
+//         * it calculates the product of the corresponding TF-IDF values
+//         * in vector1 and the term frequency in vector2, adding this to
+//         * dotProduct. It also adds the square of the TF-IDF value to norm1.
+//         */
+//        for (String term : vector1.keySet()) {
+//            double tfIdf = vector1.get(term);
+//            double tf = vector2.getOrDefault(term, 0);
+//            dotProduct += tfIdf * tf;
+//            norm1 += tfIdf * tfIdf;
+//        }
+//
+//        /**
+//         * Iterate over the term frequencies in vector2, adding
+//         * the square of each term frequency to norm2.
+//         */
+//        for (int tf : vector2.values()) {
+//            norm2 += tf * tf;
+//        }
+//
+//        /**
+//         * Finally, the method returns the cosine similarity,
+//         * which is the dot product divided by the product of
+//         * the Euclidean norms. If either of the norms is zero
+//         * (indicating a zero vector), it returns 0.0 to avoid
+//         * division by zero.
+//         */
+//        // Avoid division by zero
+//        if (norm1 == 0 || norm2 == 0) return 0.0;
+//        return dotProduct / (Math.sqrt(norm1) * Math.sqrt(norm2));
+//    }
+//
+//    public List<SearchResult> search(String query, int fromIndex, int toIndex) {
+//        Map<String, Double> queryVector = vectorize(query);
+//        List<SearchResult> results = new ArrayList<>();
+//
+//        /**
+//         * I. Retrieve List of documents based on search terms
+//         */
+//
+//        // 1. We need a list of stemmed words
+//        String[] words = getWords(query);
+//
+//        // 2. For each word, retrieve the urls from the index
+//
+//        // 3. For each url:
+//        //      - hash the url
+//        //      - Gather each map from 'pt-term-freq' table
+//        //      - get page rank
+//
+//
+//
+//        /**
+//         * II. For each document, calculate cosine similarity
+//         */
+//
+//        // 1. For every page from the index
+//        // 2.
+//
+//        for (TFIDFDocument document : documents) {
+//            Map<String, Integer> docTF = termFrequencies.get(document.getId());
+//            double cosineSimilarity = computeCosineSimilarity(queryVector, docTF);
+//
+//            results.add(new SearchResult(document, cosineSimilarity));
+//        }
+//
+//        results.sort((r1, r2) -> Double.compare(r2.getScore(), r1.getScore())); // Sort in descending order
+//        return results.subList(fromIndex, toIndex);
+//    }
+
 
 
     public static void main(String args[]) throws IOException {
