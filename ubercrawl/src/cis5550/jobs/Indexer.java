f4c61e1a3e0fab49d3c47266f0bbb7cd08a56258
Yingqiu Liu <yingqiul@seas.upenn.edu>
2023-12-04 00:43:06 -0800

    update indexer and ranker with new data model

--- ubercrawl/src/cis5550/jobs/Indexer.java
+++ ubercrawl/src/cis5550/jobs/Indexer.java
@@ -1,148 +1,175 @@
-/**
- * Indexer program
- *
- * The purpose of this job is to process every record
- * in the pt-crawl table, to extract the urls and text,
- * and from them to create a reverse index that maps
- * words to urls.
- *
- * Notice that in order to work with complex url
- * characters, it uses Base64 encoding. The document
- * text is filtered, so it does not present any issues.
- *
- * @author Sergio Garcia <gsergio@seas.upenn.edu>
- */
 package cis5550.jobs;
 
-import cis5550.flame.*;
+import cis5550.flame.FlameContext;
+import cis5550.kvs.KVSClient;
+import cis5550.kvs.Row;
 import cis5550.tools.Hasher;
-import cis5550.tools.Helpers;
+import cis5550.external.PorterStemmer;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class Indexer {
-	/**
-	 * As a first step, create the class for the indexer, and, in its run method.
-	 * @param context
-	 * @param args
-	 * @throws Exception
-	 */
-	public static void run(FlameContext context, String[] args) throws Exception {
-		HashSet<String> englishWords;
+    public static void run(FlameContext ctx, String[] args) throws Exception {
+        KVSClient kvs = ctx.getKVS();
 
-		// Load English Words if provided
-		if(args.length > 0 && args[0] != null && !args[0].isEmpty()) {
-			englishWords = Helpers.loadWordsFromFile(args[0]);
-		} else {
-			englishWords = new HashSet<>();
-		}
+        List<String> normalizedPages = new ArrayList<>();
 
-		if(englishWords.isEmpty()) {
-			context.output("English words file not found: " + args[0] + ". Needs to be first argument.");
-			return;
-		}
+        // Iterate through workers and fetch data for each row individually
+        for (int i = 0; i < kvs.numWorkers(); i++) {
+            String workerAddress = kvs.getWorkerAddress(i);
 
-		/**
-		 * Load the data from the pt-crawl table. Ideally, we would like a PairRDD of (u, p) pairs,
-		 * where u is a normalized URL and p is the contents of the corresponding page,
-		 * but we only have a fromTable for normal RDDs.
-		 */
+            // Fetch rows from pt-crawl table for the current worker
+            String tableName = "pt-crawl";
+            Iterator<Row> rowIterator = fetchRowsFromWorker(kvs, tableName, workerAddress);
+            // Process rows and perform indexing
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                String url = row.get("url");
+                String content = row.get("page");
 
+                if (url == null || content == null) continue;
 
-		/**
-		 * First, use fromTable and map each Row of the pt-crawl table to a string u,p,
-		 * where u is the URL and p the page, and then use mapToPair to convert this
-		 * into a PairRDD again.
-		 */
+                // Process rows and perform indexing
+                try {
+                    processRow(ctx, row);
+                } catch (Exception ex){
+                    System.out.println("Handle processing row exception" + ex.getMessage());
+                }
 
-		FlamePairRDD flamePairRDD = context.fromTable(
-			"pt-crawl",
-			// we would like a PairRDD of (u, p) pairs,
-			row -> {
-				return
-					row.get("url") +  // where u is the URL
-					"@" +
-					row.get("page");  // and p the page
-			}
-		)
-		// And then use mapToPair
-		.mapToPair(pairString -> {
-			try {
-				String[] pairParts = pairString.split("@", 2);
-				// It helps avoid insertion errors with strange urls
-				String url = Helpers.encode64(pairParts[0]);
-				String page = pairParts[1];
+                // Normalize the URL and create (u, p) pairs as strings
+                String normalizedUrl = normalizeUrl(url);
+                for (String s : content.split(" ")) {
+                    try {
+                        kvs.put("pt-index", s, "url", url);
+                    } catch (Exception ex) {
+                        System.out.println("Handle putting row exception" + ex.getMessage());
+                    }
+                }
 
-				if(!url.isEmpty() && !page.isEmpty()) {
-					return new FlamePair(url, page);
-				}
+                normalizedPages.add(normalizedUrl + "," + content);
+            }
+        }
 
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return new FlamePair("", "");
-		});
+        // After saving the pt-index table, fetch and print its content
+        Iterator<Row> indexTableIterator = kvs.scan("pt-index", null, null);
+        System.out.println("Content of pt-index table:");
+        while (indexTableIterator.hasNext()) {
+            Row indexRow = indexTableIterator.next();
+            String key = indexRow.key();
+            String value = indexRow.columns().iterator().next(); // Assuming there's only one column per row
+        }
 
+    }
 
-		/**
-		 * Next, create the inverted index. This involves two simple steps.
-		 * First, we need to convert each (u, p) pair to lots of (w, u) pairs,
-		 * where w is a word that occurs in p. You can use flatMapToPair for this.
-		 */
-		flamePairRDD.flatMapToPair(kvPair -> {
-				// First, we create a set of FlamePairs, which is our output
-				// A hashmap would be faster, but
-				HashSet<FlamePair> wordUrlPairs = new HashSet<>();
-				// Gather the url, and the page
-				String url = kvPair._1().trim(); // Key: url
-				String page = kvPair._2().trim(); // Value: page contents
+    private static Iterator<Row> fetchRowsFromWorker(KVSClient kvs, String tableName, String workerAddress) {
+        try {
+            return kvs.scan(tableName);
+        } catch (IOException e) {
+            // Handle the exception (e.g., log the error)
+            e.printStackTrace();
+            return Collections.emptyIterator();
+        }
+    }
 
-				/**
-				 * Make sure to filter out all HTML tags, to remove all punctuation,
-				 * and to convert everything to lower case.
-				 */
-				// Filter out all html tags
-				page = page.replaceAll("<[^>]*>", "");
-				// Remove all punctuation
-				page = page.replaceAll("\\p{Punct}", ""); // !”#$%&'()*+,-./:;<=>?@[\]^_`{|}~:
-				// convert everything to lower case
-				page = page.toLowerCase();
+    private static void processRow(FlameContext ctx, Row row) {
+//        if (row.get("url") == null || row.get("page") == null || row.get("contentType") == null) return;
+        String url = row.get("url");
+        byte[] contentBytes = row.get("page").getBytes();
+        String contentType = row.get("contentType");
 
-				String[] words = Helpers.getWords(page, englishWords);
+        if (contentType != null && contentType.startsWith("text/html")) {
+            String content = new String(contentBytes, StandardCharsets.UTF_8);
+            String hashedUrl = Hasher.hash(url);
 
-				/**
-				 * The resulting PairRDD will still contain lots of (w, ui) pairs,
-				 * with the same word w but different URLs ui.
-				 */
-				// Break the page contents into words
-				Arrays.stream(words).forEach(word -> {
-					if(url != null && !url.isEmpty()) {
-						// For each word, crate a unique pair. Uniqueness guaranteed by HashSet.
-						wordUrlPairs.add(
-							// Note that the word is now hashed
-							new FlamePair(Hasher.hash(word), url)
-						);
-					}
-				});
-				// Output the unique set for folding
-				return wordUrlPairs;
-			})
-			/**
-			 * We’ll need to fold all the URLs into a single
-			 * comma-separated list, using foldByKey.
-			 */
-			.foldByKey(
-				"",
-				(a, b) ->
-					// Lastly we just have to join with a comma
-					a.isEmpty()
-						? a + b // If empty, we just put them together
-						: a + "," + b // If not, then separate
-			)
-			/**
-			 * This should produce the required data; you can rename the final PairRDD
-			 * to pt-index using the saveAsTable method.
-			 */
-			.saveAsTable("pt-index");
-	}
+            String indexedContent = performIndexing(content, ctx);
+            System.out.println("Indexed Content: " + indexedContent);
+
+            // Store indexed data using KVS
+            try {
+                System.out.println("Hashed URL: " + hashedUrl);
+                ctx.getKVS().put("pt-index", row.get("page"), "indexedContent", indexedContent);
+            } catch (IOException e) {
+                // Handle the exception (e.g., log the error)
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static String performIndexing(String content, FlameContext ctx) {
+        String[] words = content.split("\\s+|\\p{Punct}");
+        PorterStemmer stemmer = new PorterStemmer(); // Instantiate the stemmer
+
+        Map<String, Set<String>> wordIndex = new HashMap<>();
+        StringBuilder indexedContent = new StringBuilder();
+
+        for (String word : words) {
+            // Normalize the word to lowercase and skip short words
+            word = word.toLowerCase();
+            if (word.length() > 2) {
+                // Remove any non-alphabetic characters from the word
+                word = word.replaceAll("[^a-zA-Z]", "");
+                // Skip empty words after removing non-alphabetic characters
+                if (!word.isEmpty()) {
+                    // Stem the word using PorterStemmer
+                    stemmer.add(word.toCharArray(), word.length());
+                    stemmer.stem();
+                    String stemmedWord = stemmer.toString();
+
+                    // Update the index map with the original word
+                    wordIndex.computeIfAbsent(word, k -> new HashSet<>()).add(word);
+                    // Update the index map with the stemmed word
+                    wordIndex.computeIfAbsent(stemmedWord, k -> new HashSet<>()).add(word);
+
+                    // Now add the original and stemmed words to the KVS
+                    try {
+                        System.out.println("Indexing word: " + word + " (Stemmed: " + stemmedWord + ")");
+                        ctx.getKVS().put("pt-index", word, "indexedContent", String.valueOf(indexedContent));
+                        ctx.getKVS().put("pt-index", stemmedWord, "indexedContent", String.valueOf(indexedContent));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+
+        // Build the indexed content as a string
+        for (Map.Entry<String, Set<String>> entry : wordIndex.entrySet()) {
+            String word = entry.getKey();
+            Set<String> urls = entry.getValue();
+
+            // Format: word:url1,url2,url3,...
+            String indexedWord = word + ":" + String.join(",", urls);
+
+            // Append the indexed word to the indexed content
+            indexedContent.append(indexedWord).append(" ");
+        }
+
+        // Trim the trailing space and return the indexed content
+        return indexedContent.toString().trim();
+    }
+
+    private static String normalizeUrl(String url) {
+        // Convert the URL to lowercase
+        String normalizedUrl = url.toLowerCase();
+
+        int fragmentIndex = normalizedUrl.indexOf('#');
+        if (fragmentIndex != -1) {
+            normalizedUrl = normalizedUrl.substring(0, fragmentIndex);
+        }
+
+        // Remove trailing slash (except for the root URL)
+        if (normalizedUrl.endsWith("/") && normalizedUrl.length() > 1) {
+            normalizedUrl = normalizedUrl.substring(0, normalizedUrl.length() - 1);
+        }
+
+        // Remove default port numbers (e.g., http://example.com:80/)
+        normalizedUrl = normalizedUrl.replaceAll(":80/", "/");
+
+        // Remove www subdomain (optional, based on preference)
+        normalizedUrl = normalizedUrl.replaceFirst("www\\.", "");
+
+        return normalizedUrl;
+    }
 }
